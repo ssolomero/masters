f0a635174e90bc0046b0fcd9c44a6b1a942c8387
Sharence Solomero <ssolomero@gmail.com>
2025-07-31 12:23:34 -0500

    remove console

--- app/lib/config/db.ts
+++ app/lib/config/db.ts
@@ -4,12 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 if (!process.env.NEXT_PUBLIC_MONGODB_URI) {
-  console.log(process.env);
   throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
 const URI = process.env.NEXT_PUBLIC_MONGODB_URI;
-console.log(process.env);
 
 const client = new MongoClient(URI, {
   serverApi: {
